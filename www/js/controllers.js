f1faf9cd12cebd5f8ef600f7dc585ec8b4937021
Paolocargnin <cargnin.paolo@gmail.com>
2014-12-10 16:06:28 +0100

    first commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,5 +11,15 @@ angular.module('starter.controllers', [])
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('CreateCtrl', function($scope) {
+	document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady() {
+	    console.log(navigator.camera);
+	}
+	$scope.photo= function(){
+		// navigator.camera.getPicture( cameraSuccess, cameraError, cameraOptions );
+		navigator.camera.getPicture( function(cameraSuccess){
+			debugger;
+		}, cameraError, cameraOptions );
+	};
 });
