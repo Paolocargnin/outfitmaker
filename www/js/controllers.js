4ac680d642de349fc7dc2ed6632c40e28b2b2c8f
Paolo Cargnin <cargnin.paolo@gmail.com>
2014-12-22 19:27:00 +0100

    iniziamo a fare sul serio

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,18 @@ angular.module('starter.controllers', [])
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('CreateCtrl', function($scope,$location,Camera) {
+.controller('CreateCtrl', function($scope,$location,$timeout,$localstorage,Camera) {
+	$scope.init=function(){
+		$scope.dresses=$localstorage.getObject('dresses');
+
+		$scope.imageBox={
+			imageUrl:'',
+			show: function(string){
+				$timeout(function(){$scope.imageBox.imageUrl=string;},0);
+			}
+		}
+	};
+
 	$scope.getPhoto = function() {
 		Camera.getPicture({
 		}).then(function(imageURI) {
@@ -19,11 +30,6 @@ angular.module('starter.controllers', [])
 			Camera.lastPhoto = imageURI;
 
 			$location.path('/tab/create/add/photo');	
-			// setTimeout(function() {
-			// 	$('.imageToAdd').attr('width',400);
-			// 	$('.imageToAdd').attr('height',200);
-			// 	polyClip.init();
-			// }, 1200);
 		}, function(err) {
 			console.err(err);
 		}, {
@@ -33,11 +39,18 @@ angular.module('starter.controllers', [])
 		});
 	};
 })
-.controller('CreatePhotoCtrl', function($scope,Camera) {
+.controller('CreatePhotoCtrl', function($scope, $timeout, $localstorage, Camera) {
 	$scope.cropDots=[];
+	$scope.dress={
+		name:'',
+		category:'',
+		imageUrl:'',
+	}
+
 
 	$scope.init=function(){
 		$scope.notTapped=true;
+		$scope.cropped=false;
 		$scope.startDotsAnimation();
 
 		$scope.cropArea=document.querySelector('.crop-area');
@@ -124,6 +137,42 @@ angular.module('starter.controllers', [])
 		$scope.contextCropArea.stroke();
 	};
 
+	$scope.findCroppedLeftTop=function(){
+		var ret= {};
+		$scope.cropDots.forEach(function(dot,i){
+			if (i == 0){
+				ret= {
+					minLeft:dot.naturalLeft,
+					maxLeft:dot.naturalLeft,
+					minTop:dot.naturalTop,
+					maxTop:dot.naturalTop,
+				}
+			}
+
+			if (ret.minLeft>dot.naturalLeft){
+				ret.minLeft=dot.naturalLeft;
+			}
+			if ( ret.maxLeft < dot.naturalLeft){
+				ret.maxLeft=dot.naturalLeft;
+			}
+
+			if (ret.minTop>dot.naturalTop){
+				ret.minTop = dot.naturalTop;
+			}
+			if ( ret.maxTop < dot.naturalTop){
+				ret.maxTop = dot.naturalTop;
+			}
+		});
+		return ret;
+	};
+
+	$scope.findCroppedCanvasSize= function(ret){
+		return {
+			width: ret.maxLeft-ret.minLeft,
+			height: ret.maxTop-ret.minTop,
+		}
+	}
+
 	$scope.cropCanvas=function(){
 		$scope.contextCropArea.closePath();
 
@@ -131,12 +180,17 @@ angular.module('starter.controllers', [])
 
 		var cropString= '';
 		$scope.cropDots.forEach(function(dot,index){
-			var left= ($scope.image.naturalWidth*dot.left)/$scope.image.width,
-				top=($scope.image.naturalHeight*dot.top)/$scope.image.height;
+			var naturalLeft= ($scope.image.naturalWidth*dot.left)/$scope.image.width,
+				naturalTop=($scope.image.naturalHeight*dot.top)/$scope.image.height;
+			
+			dot.naturalLeft=naturalLeft;
+			dot.naturalTop=naturalTop;
+
 			if (index!=0){
 				cropString=cropString+=",";
 			}
-			cropString=cropString+left+','+top;
+
+			cropString=cropString+naturalLeft+','+naturalTop;
 		});
 		
 
@@ -144,12 +198,52 @@ angular.module('starter.controllers', [])
 
 		polyClip.addCallback(function(){
 			//Load the third step
-			$scope.saveCanvas();
+			$timeout(function(){$scope.cropped=true;},0);
+			$scope.cropArea.removeChild($scope.cropMaker);
+			$('.crop-dot').remove();
+
+			$scope.croppedCanvas=document.querySelector('canvas#polyClip0');
+
+			$scope.croppedImage = new Image();
+			$scope.croppedImage.src=$scope.croppedCanvas.toDataURL();
+
+			var canvasCroppedLeftTop=$scope.findCroppedLeftTop(),
+				canvasSize= $scope.findCroppedCanvasSize(canvasCroppedLeftTop);
+
+			var ctx= $scope.croppedCanvas.getContext("2d");
+			ctx.clearRect ( 0 , 0 , $scope.croppedCanvas.width, $scope.croppedCanvas.height );
+
+			$scope.croppedCanvas.width=canvasSize.width;
+			$scope.croppedCanvas.height=canvasSize.height;
+
+			ctx.clearRect ( 0 , 0 , canvasSize.width, canvasSize.height );
+
+			ctx.drawImage($scope.croppedImage, canvasCroppedLeftTop.minLeft * -1 , canvasCroppedLeftTop.minTop * -1 );
+			// $scope.saveCanvas();
 		});
 		polyClip.init();
 		// $('.imageToAdd').removeClass('auto-width')
 	}
+	
+	$scope.rotateCanvas= function(degrees){
+		$scope.croppedImage = new Image();
+		$scope.croppedImage.src=$scope.croppedCanvas.toDataURL();
 
+		var ctx= $scope.croppedCanvas.getContext("2d");
+		ctx.clearRect ( 0 , 0 , $scope.croppedCanvas.width, $scope.croppedCanvas.height );
+
+		//And now, re-put the image on the canvas, but roteting it by 90 degrees!!!! Oh fuck yeah bitch I love to write english comments.
+
+		var y = $scope.croppedCanvas.width;
+		$scope.croppedCanvas.width=$scope.croppedCanvas.height;
+		$scope.croppedCanvas.height=y;
+
+		ctx.translate($scope.croppedCanvas.width/2,$scope.croppedCanvas.height/2);
+		ctx.rotate(degrees*Math.PI/180);
+
+		ctx.drawImage($scope.croppedImage, $scope.croppedCanvas.height/2*-1, $scope.croppedCanvas.width/2*-1);
+	};
+	
 	$scope.saveCanvas= function(){
 		var canvasEl=document.querySelector('.polyClip-clipped');
 
@@ -157,8 +251,11 @@ angular.module('starter.controllers', [])
 
 		   	window.canvas2ImagePlugin.saveImageDataToLibrary(
 				function(msg) {
+					$scope.dress.imageUrl=msg;
+					$scope.storeDress();
+
 					navigator.notification.alert(
-						msg,
+						'Dress saved',
 						function(){},
 						"Complete",
 						"OK");
@@ -170,7 +267,19 @@ angular.module('starter.controllers', [])
 				document.querySelector('.polyClip-clipped')
 		    );
 		}else{
-			// do nothing
+			$scope.dress.imageUrl=$scope.croppedImage.src;
+			$scope.storeDress();
 		}
+	};
+
+	$scope.storeDress = function(){
+
+		//Va fatto un servizio. Sia chiaro
+		var dresses = $localstorage.getObject('dresses');
+		if (dresses.items==undefined){
+			dresses.items=[];
+		}
+		dresses.items.push($scope.dress);
+		$localstorage.setObject('dresses',dresses);
 	};
 });
